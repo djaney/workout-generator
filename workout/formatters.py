b2c95fd4adcc6c8576c57a85645344e9fb41e2c4
djane <d.mabelin@arcanys.com>
2020-04-22 07:22:00 +0800

    add some exercise templates

--- workout/formatters.py
+++ workout/formatters.py
@@ -9,11 +9,14 @@ class CliFormatter(Formatter):
     def format(self, workouts, template):
         workout_list = []
         for e in workouts:
-            workout_list.append("  - {} {}".format(e[0][0], e[1]))
+            name = e[0][0]
+            reps = e[1]
+            sets = self.generator.sets
+            workout_list.append("  - {} {} x {}".format(name, reps, sets))
         workout_list = "\n".join(workout_list)
 
         pre_text = "## {}".format(template['title'] if 'title' in template else "")
-        post_text = "Do {} sets of each exercise".format(self.generator.sets)
+        post_text = "<exercise> <reps> x <sets>"
 
         return pre_text + "\n\n" + workout_list + "\n\n" + post_text
 
--- workout/templates.py
+++ workout/templates.py
@@ -4,10 +4,11 @@ from .exercises import (
     T_CORE,
     T_CARDIO
 )
+import random
 
 
-def get_random_template():
-    return {
+TEMPLATES = {
+    "balanced": {
         "title": "Balanced workout",
         "exercises": [
             (T_UPPER, 1),
@@ -15,5 +16,35 @@ def get_random_template():
             (T_CORE, 1),
             (T_CARDIO, 1),
         ]
+    },
+    "upper-body": {
+        "title": "Upper body focused",
+        "exercises": [
+            (T_UPPER, 1),
+            (T_UPPER, 1),
+            (T_LOWER, 1),
+            (T_CORE, 1),
+            (T_CORE, 1),
+            (T_CARDIO, 1),
+        ]
+    },
+    "lower-body": {
+        "title": "Lower body focused",
+        "exercises": [
+            (T_LOWER, 1),
+            (T_LOWER, 1),
+            (T_UPPER, 1),
+            (T_CORE, 1),
+            (T_CORE, 1),
+            (T_CARDIO, 1),
+        ]
     }
+}
+
+def get_random_template():
+    return random.choice(list(TEMPLATES.values()))
+
+
+def get_template(key):
+    return TEMPLATES[key]
 
