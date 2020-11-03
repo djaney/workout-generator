90c9560dac9239c361019b3f3637ee2aa0f7a727
djane <d.mabelin@arcanys.com>
2020-11-03 11:04:07 +0800

    updates

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='workout-gen',
-    version='0.1.0',
+    version='1.0.0',
     description='Generates some workout for the day',
     author='Djane Mabelin',
     author_email='thedjaney@gmail.com',
